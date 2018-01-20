1bd2834f04f87eeb866384e13ee3de9e4b220029
Andrei Ausianik <andrei.ausianik@iii.com>
2018-01-20 13:50:00 +0300

    frogs-race task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getBestRacer(distance) {
-  // your solution here
+  const log = a => {
+    console.log(a);
+    return a;
+  }
+  return distance.map((line) => {
+    for(let step = 1, i = 0; step <= line.length && i < line.length; step++) {
+      i+= line[i];
+      if(i >= line.length) {
+        return step;
+      }
+      if(i < 0) {
+        return Infinity;
+      }
+    }
+    return Infinity;
+  }).reduce((bestIndex, item, index, arr) => item < arr[bestIndex] ? index : bestIndex, 0);  
 }
